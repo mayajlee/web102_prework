110efb9a3ad1cffb024ae725036fecf4f04dcbb4
mayajlee <mlee15@syr.edu>
2023-11-23 10:36:34 -0500

    finished!

--- index.js
+++ index.js
@@ -29,27 +29,33 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for(let i = 0; i < games.length; i++){
 
         // create a new div element, which will become the game card
-
-
+        const gameDiv = document.createElement("div");
+        
         // add the class game-card to the list
-
+        gameDiv.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
-
+        const display = `
+            <img src = ${games[i].img} class= "game-img" />
+            <h1>${games[i].name}</h1>
+            <p>${games[i].description}</p>
+            <p>Backers: ${games[i].backers}`;
+        
+        gameDiv.innerHTML = display;
         // append the game to the games-container
-
+        gamesContainer.append(gameDiv);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
@@ -61,20 +67,20 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+let totalContributions = GAMES_JSON.reduce((sum, game) => sum += game.backers,  0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `<h3> ${totalContributions} <h3>`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
-
+let totalRaised = GAMES_JSON.reduce((sum, game) => sum += game.pledged,  0);
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `<h3>$ ${totalRaised} <h3>`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+gamesCard.innerHTML = `<h3> ${GAMES_JSON.length} <h3>`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
@@ -87,9 +93,11 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    const filterGames = GAMES_JSON.filter((game) => game.pledged < game.goal);
 
     // use the function we previously created to add the unfunded games to the DOM
+    console.log(filterGames.length);
+    addGamesToPage(filterGames);
 
 }
 
@@ -98,10 +106,11 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const filterGames = GAMES_JSON.filter((game) => game.pledged >= game.goal);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    console.log(filterGames.length);
+    addGamesToPage(filterGames);
 }
 
 // show all games
@@ -109,7 +118,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -118,7 +127,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames); 
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
@@ -129,13 +140,17 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const filterGames = GAMES_JSON.filter((game) => game.pledged < game.goal);
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const displayStr = filterGames.length > 1 ? `A total of ${totalContributions} has been raised for ${GAMES_JSON.length - filterGames.length}.
+${filterGames.length} games remain unfunded. We need your help to fund these amazing games!` : `A total of ${totalContributions} has been raised for ${GAMES_JSON.length - 1}.
+1 game remains unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
-
+const actionStatement = document.createElement("p");
+actionStatement.innerHTML = displayStr;
+descriptionContainer.append(actionStatement);
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
  * Skills used: spread operator, destructuring, template literals, sort 
@@ -149,7 +164,16 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [gameOne, gameTwo, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const gameOneName = document.createElement("p");
+gameOneName.innerHTML = gameOne.name;
+firstGameContainer.append(gameOneName);
+
+const gameTwoName = document.createElement("p");
+gameTwoName.innerHTML = gameTwo.name;
+secondGameContainer.append(gameTwoName);
+
 
 // do the same for the runner up item
